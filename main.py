32c87233be6ead3ab9cfcd2a6a5f7ef1b63a7fe2
yah <yassin.azouagh@fedasil.be>
2024-12-07 00:28:53 +0100

    split code

--- main.py
+++ main.py
@@ -1,41 +1,42 @@
-from flask import Flask, render_template_string
-from flask_socketio import SocketIO
-import time
-import threading
 import os
-app = Flask(__name__)
-socketio = SocketIO(app)
+import time
+from threading import Thread
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
+app = Flask(__name__)
+
+# Configuration de SocketIO
+app.config['SECRET_KEY'] = os.urandom(24)
+socketio = SocketIO(app, cors_allowed_origins="*")  # Permet toutes les origines (ajuste selon tes besoins)
+
+# Initialisation du compteur
 counter = 0
 
-def counter_loop():
+@app.route('/')
+def index():
+    return render_template('index.html')  # Crée un fichier index.html dans le dossier templates
+
+# Fonction pour incrémenter le compteur automatiquement
+def increment_counter():
     global counter
     while True:
-        time.sleep(5)
+        time.sleep(5)  # Attendre 5 secondes
         counter += 1
-        socketio.emit('update_counter', {'counter': counter})  # Envoie la mise à jour du compteur aux clients connectés
+        socketio.emit('counter_update', {'counter': counter}, room=None)  # Diffuser à tous les clients
 
-@app.route("/")
-def home():
-    return render_template_string("""
-        <html>
-            <head>
-                <title>Counter</title>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.0.1/socket.io.min.js"></script>
-                <script type="text/javascript">
-                    // Connexion au serveur WebSocket
-                    var socket = io.connect('http://127.0.0.1:5000');
-                    socket.on('update_counter', function(data) {
-                        document.getElementById('counter').innerText = "Counter is at: " + data.counter;
-                    });
-                </script>
-            </head>
-            <body>
-                <h1 id="counter">Counter is at: {{ counter }}</h1>
-            </body>
-        </html>
-    """, counter=counter)
+@socketio.on('increment')
+def handle_increment():
+    global counter
+    counter += 1
+    emit('counter_update', {'counter': counter}, broadcast=True)  # Envoie la nouvelle valeur du compteur à tous les clients
 
 if __name__ == "__main__":
+    # Démarrer un thread pour incrémenter le compteur toutes les 5 secondes
+    thread = Thread(target=increment_counter)
+    thread.daemon = True
+    thread.start()
+
+    # Lancer l'application Flask
     port = os.getenv('PORT', 5000)  # Utilise le port fourni par Render, sinon 5000 par défaut
     socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
