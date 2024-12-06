c2f71a8282c40a0eb818a294f487439838c07d35
yah <yassin.azouagh@fedasil.be>
2024-12-06 23:40:43 +0100

    update

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+from flask import Flask
+import threading
 import time
 
+app = Flask(__name__)
 counter = 0
 
-while True:
-    counter += 1
-    print(f"Counter: {counter}")
-    time.sleep(5)
+def counter_loop():
+    global counter
+    while True:
+        counter += 1
+        print(f"Counter: {counter}")
+        time.sleep(5)
+
+@app.route("/")
+def home():
+    return f"Counter is at: {counter}"
+
+if __name__ == "__main__":
+    threading.Thread(target=counter_loop, daemon=True).start()
+    app.run(host="0.0.0.0", port=5000)
