2fb3cb8fdc373a897ef330f1da8cfccea11e4256
yah <yassin.azouagh@fedasil.be>
2024-12-07 01:44:43 +0100

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import threading
 import time
+import os
 
 app = Flask(__name__)
 
@@ -24,4 +25,6 @@ def index():
     return render_template('index.html', counter=counter)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Utilisation du port fourni par Render, ou 5000 par défaut
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
